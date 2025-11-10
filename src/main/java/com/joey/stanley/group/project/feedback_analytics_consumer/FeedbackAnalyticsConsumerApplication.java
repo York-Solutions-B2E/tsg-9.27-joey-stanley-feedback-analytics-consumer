bc14ad3e0e288bd720eb2c6f64878f2350c95aba
stan10aviles <stan10aviles@hotmail.com>
2025-11-10 12:49:31 -0500

    Initialized spring boot feedback analytics consumer backend

---
+++ src/main/java/com/joey/stanley/group/project/feedback_analytics_consumer/FeedbackAnalyticsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.joey.stanley.group.project.feedback_analytics_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAnalyticsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAnalyticsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joey/stanley/group/project/feedback_analytics_consumer/FeedbackAnalyticsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.stanley.group.project.feedback_analytics_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAnalyticsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
