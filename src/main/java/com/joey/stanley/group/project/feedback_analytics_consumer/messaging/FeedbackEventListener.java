79c065321ff788cb4fb7c790213e018c006c0bc2
stan10aviles <stan10aviles@hotmail.com>
2025-11-17 20:59:29 -0500

    Added logger to meet project spec. swapped back to regular JsonDeserializer.

--- src/main/java/com/joey/stanley/group/project/feedback_analytics_consumer/messaging/FeedbackEventListener.java
+++ src/main/java/com/joey/stanley/group/project/feedback_analytics_consumer/messaging/FeedbackEventListener.java
@@ -1,15 +1,28 @@
 package com.joey.stanley.group.project.feedback_analytics_consumer.messaging;
 
 import com.joey.stanley.group.project.feedback_analytics_consumer.dtos.FeedbackSubmittedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedbackEventListener {
+
+    private static final Logger log = LoggerFactory.getLogger(FeedbackEventListener.class);
+
     @KafkaListener(
             topics = "feedback-submitted",
             groupId = "feedback-analytics-group")
     public void feedbackListener(FeedbackSubmittedEvent event) {
-        System.out.println("Received feedback event: " + event);
+        log.info("Received feedback: id={} memberId={} providerName={} rating={} comment={} submittedAt={} schemaVersion={}",
+                event.getId(),
+                event.getMemberId(),
+                event.getProviderName(),
+                event.getRating(),
+                event.getComment(),
+                event.getSubmittedAt(),
+                event.getSchemaVersion()
+        );
     }
 }
